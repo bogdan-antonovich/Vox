21d4748dbbe920e050d6d8061a335a15a1be89b8
l1qwie <programjibogdan@gmail.com>
2026-04-12 19:47:31 +0400

    :D

--- vox_api/internal/hub/texttoaudio-2.go
+++ vox_api/internal/hub/texttoaudio-2.go
@@ -52,8 +52,8 @@ func (f *OpenAI) Handle(ctx context.Context, s Stream) error {
 	return nil
 }
 
-// wtf
 func (f *OpenAI) NewStream(ctx context.Context) (s Stream, err error) {
+	// I dont care
 	f.log.Debug("OpenAI.NewStream", zap.Bool("ctx_is_nil", ctx == nil))
 	response, err := f.client.Audio.Speech.New(ctx, openai.AudioSpeechNewParams{ //nolint:bodyclose
 		Model:          openai.SpeechModelTTS1,
